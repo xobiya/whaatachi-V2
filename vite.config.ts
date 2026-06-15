9723b6ae5014bd973bbcad6c0015e6eb25487014
Xobiya <eshetufeleke21@gmail.com>
2026-06-15 08:30:36 +0300

    chore: update proxy target port to 3001 in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig(() => {
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
       proxy: {
         '/api': {
-          target: 'http://localhost:3000',
+          target: 'http://localhost:3001',
           changeOrigin: true,
         },
       },
